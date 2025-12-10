62ed39be249e853bec6584d74318587879a30cab
Elena Badillo Goicoechea <elenabadillog@uchicago.edu>
2025-12-09 17:43:47 -0800

    Add files via upload

--- 2025/aoc_1.py
+++ 2025/aoc_1.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv("input.csv")
+df = pd.read_csv("1_input.csv")
 nlist = list(df["path"])
+print(nlist)
 
-def get_pwd(nlist):
+
+def get_pwd(nlist):  # part 1
     cnt = 0
     init_n = 50
     for n in nlist:
@@ -11,14 +13,38 @@ def get_pwd(nlist):
         k = float(n[1:])
         if s == "L":
             init_n = init_n - k
-            if (init_n % 100 == 0):
+            if init_n % 100 == 0:
                 cnt += 1
         elif s == "R":
             init_n = init_n + k
-            if (init_n % 100 == 0):
+            if init_n % 100 == 0:
                 cnt += 1
     print("A: " + str(cnt))
     return cnt
 
-if 
-%timeit get_pwd(nlist)
+
+def get_pwd_2(nlist):  # part 2
+    cnt = 0
+    init_n = 50
+    for n in nlist:
+        s = n[0]
+        k = float(n[1:])
+
+        if s == "L":
+            for i in range(int(init_n) - 1, int(init_n - k) - 1, -1):
+                if i % 100 == 0:
+                    cnt += 1
+
+            init_n -= k
+        elif s == "R":
+            for i in range(int(init_n) + 1, int(init_n + k) + 1):
+                if i % 100 == 0:
+                    cnt += 1
+            init_n += k
+
+    print("B: " + str(cnt))
+    return cnt
+
+
+get_pwd(nlist)
+get_pwd_2(nlist)
